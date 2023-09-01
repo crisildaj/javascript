bdb92b24089e061869f64a20ae76f2c6d57b79a1
Crisilda Jojo <crisildajo@gmail.com>
2023-09-01 13:50:36 +0100

    Exercise 3b completed

---
+++ loops.js
@@ -0,0 +1,17 @@
+let i=1
+for (i; i<10; i+=1) {
+    console.log(i);
+}
+
+let x=2
+let loopCounter=0
+// let loopCounter=4
+while (x<10000) {
+    x=x**2
+    // countdown
+    // loopCounter -=1
+    loopCounter +=1
+    console.log("x:", x)
+    console.log("Loop Counter:", loopCounter)
+
+}
